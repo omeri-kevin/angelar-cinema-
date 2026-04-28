ecb1bbb419778eba1be07c77a766b02bfd9a2c51
omeri-kevin <omeri.kevin@iisgalvanimi.edu.it>
2026-04-28 18:06:24 +0000

    commit finale

--- cinemaa/src/app/app.ts
+++ cinemaa/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MediaList } from './media-list/media-list';
 
 @Component({
   selector: 'app-root',
