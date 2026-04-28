67d5445a4491cce4f1ec4ef0cdeb938b0ae35486
omeri-kevin <omeri.kevin@iisgalvanimi.edu.it>
2026-04-28 17:46:36 +0000

    html e media details

--- cinemaa/src/app/media-detail/media-detail.ts
+++ cinemaa/src/app/media-detail/media-detail.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-
+import { Input } from '@angular/core';
+import { Media } from '../models/media-model';
 @Component({
   selector: 'app-media-detail',
   imports: [],
   templateUrl: './media-detail.html',
   styleUrl: './media-detail.css',
 })
-export class MediaDetail {}
+export class MediaDetail {
+  @Input() media: Media | undefined;
+}
